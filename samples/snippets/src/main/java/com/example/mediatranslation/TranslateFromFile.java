b68c59392e14c32af71f5d2b091be12f39b451a7
Mike Ganbold <munkhbayarg@google.com>
2020-03-24 15:35:05 -0700

    added translate from the mic

--- samples/snippets/src/main/java/com/example/mediatranslation/TranslateFromFile.java
+++ samples/snippets/src/main/java/com/example/mediatranslation/TranslateFromFile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google Inc.
+ * Copyright 2020 Google LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +33,9 @@ import java.nio.file.Paths;
 
 public class TranslateFromFile {
 
+  public static void main(String[] args) throws IOException {
+    translateFromFile("resources/audio.raw");
+  }
   public static void translateFromFile() throws IOException {
     // TODO(developer): Replace these variables before running the sample.
     String filePath = "path/to/audio.raw";
@@ -40,6 +43,9 @@ public class TranslateFromFile {
   }
 
   public static void translateFromFile(String filePath) throws IOException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
     try (SpeechTranslationServiceClient speechTranslationServiceClient =
         SpeechTranslationServiceClient.create()) {
       Path path = Paths.get(filePath);
